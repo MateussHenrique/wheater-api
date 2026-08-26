f569922619974e8052ac5d19b791e9d14648afcb
Mateus Henrique <mateuzhenriq3@gmail.com>
2026-08-25 23:52:14 -0300

    feat: iniciando projeto Weather API

---
+++ src/main/java/com/mateus/wheaterapi/WheaterapiApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.wheaterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheaterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheaterapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/wheaterapi/WheaterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.wheaterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheaterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
